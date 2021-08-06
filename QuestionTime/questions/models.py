ae8e08ac3f2aff44046cf916c2f54b76339c6834
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-08-06 15:20:37 +0200

    Add questions serializers

--- QuestionTime/questions/models.py
+++ QuestionTime/questions/models.py
@@ -8,11 +8,11 @@ class Question(models.Model):
     Each question is identified by the 'slug' field.
     """
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions')
     content = models.CharField(max_length=240)
     slug = models.SlugField(max_length=255, unique=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='questions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.content
@@ -25,12 +25,12 @@ class Answer(models.Model):
     Any user can vote on any answer.
     """
 
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    body = models.TextField()
+    voters = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='votes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    body = models.TextField()
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    voters = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='votes')
 
     def __str__(self):
         return self.author.username
